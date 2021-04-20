79ba3971ca35c8a276c80b4eb96516b2aa601b05
Jordan Marr <jmarr@microdesk.com>
2021-04-20 01:18:03 -0400

    Added build/SqlHydra.InTest.props

--- src/SqlHydra/SqlServerGenerator.fs
+++ src/SqlHydra/SqlServerGenerator.fs
@@ -6,9 +6,6 @@ open FsAst
 module Schema = 
     open Microsoft.Data.SqlClient
     open System.Data
-    open Microsoft.Data.SqlClient
-    open System
-    open System.Data
     
     type Table = {
         Catalog: string
@@ -100,61 +97,61 @@ type SqlServerGenerator() =
         member __.ValidInputExtensions = 
             seq { ".toml" }
 
-        member __.Generate(ctx: GeneratorContext) =
-            let let42 =
-                SynModuleDecl.CreateLet
-                    [ { SynBindingRcd.Let with
-                            Pattern = SynPatRcd.CreateLongIdent(LongIdentWithDots.CreateString "fourtyTwo", [])
-                            Expr = SynExpr.CreateConst(SynConst.Int32 42) } ]
+        //member __.Generate(ctx: GeneratorContext) =
+        //    let let42 =
+        //        SynModuleDecl.CreateLet
+        //            [ { SynBindingRcd.Let with
+        //                    Pattern = SynPatRcd.CreateLongIdent(LongIdentWithDots.CreateString "fourtyTwo", [])
+        //                    Expr = SynExpr.CreateConst(SynConst.Int32 42) } ]
 
-            let componentInfo = SynComponentInfoRcd.Create [ Ident.Create "example1" ]
-            let nestedModule = SynModuleDecl.CreateNestedModule(componentInfo, [ let42 ])
+        //    let componentInfo = SynComponentInfoRcd.Create [ Ident.Create "example1" ]
+        //    let nestedModule = SynModuleDecl.CreateNestedModule(componentInfo, [ let42 ])
+
+        //    let namespaceOrModule =
+        //        { SynModuleOrNamespaceRcd.CreateNamespace(Ident.CreateLong "TestNS")
+        //            with Declarations = [ nestedModule ] }
+
+        //    [ namespaceOrModule ]
+
+        member __.Generate(ctx: GeneratorContext) =
+            let cs = "Data Source=localhost\SQLEXPRESS;Initial Catalog=AdventureWorksLT2019;Integrated Security=SSPI;"
+            let tables, columns = Schema.getSchema cs
+            
+            let columnsByTable =
+                columns
+                |> List.groupBy (fun c -> c.TableCatalog, c.TableSchema, c.TableName)
+                |> Map.ofList
+
+            let recordsBySchema = 
+                tables
+                |> List.groupBy (fun t -> t.Schema)
+                |> List.map (fun (schema, tables) -> 
+                    schema, 
+                        tables 
+                        |> List.map (fun table ->
+                            let tableColumns = columnsByTable.[table.Catalog, table.Schema, table.Name]
+                            Gen.toRecord (table, tableColumns)
+                        )
+                )
+            
+            let nestedModules = 
+                recordsBySchema
+                |> List.map (fun (schema, records) -> 
+                    let moduleCmpInfo = SynComponentInfoRcd.Create [ Ident.Create schema ]
+                    SynModuleDecl.CreateNestedModule(moduleCmpInfo, records)
+                )
+
+            // Get ssdt config
+            let config = ctx.ConfigGetter "ssdt"
+            // Get namespace
+            let ns = 
+                config 
+                |> Seq.tryFind(fun (key, value) -> key = "namespace")
+                |> Option.map (fun (key, value) -> string value)
+                |> Option.defaultValue "Ssdt"
 
             let namespaceOrModule =
-                { SynModuleOrNamespaceRcd.CreateNamespace(Ident.CreateLong "TestNS")
-                    with Declarations = [ nestedModule ] }
+                { SynModuleOrNamespaceRcd.CreateNamespace(Ident.CreateLong ns)
+                    with Declarations = nestedModules }
 
             [ namespaceOrModule ]
-
-        //member __.Generate(ctx: GeneratorContext) =
-            //let cs = "Data Source=localhost\SQLEXPRESS;Initial Catalog=AdventureWorksLT2019;Integrated Security=SSPI;"
-            //let tables, columns = Schema.getSchema cs
-            
-            //let columnsByTable =
-            //    columns
-            //    |> List.groupBy (fun c -> c.TableCatalog, c.TableSchema, c.TableName)
-            //    |> Map.ofList
-
-            //let recordsBySchema = 
-            //    tables
-            //    |> List.groupBy (fun t -> t.Schema)
-            //    |> List.map (fun (schema, tables) -> 
-            //        schema, 
-            //            tables 
-            //            |> List.map (fun table ->
-            //                let tableColumns = columnsByTable.[table.Catalog, table.Schema, table.Name]
-            //                Gen.toRecord (table, tableColumns)
-            //            )
-            //    )
-            
-            //let nestedModules = 
-            //    recordsBySchema
-            //    |> List.map (fun (schema, records) -> 
-            //        let moduleCmpInfo = SynComponentInfoRcd.Create [ Ident.Create schema ]
-            //        SynModuleDecl.CreateNestedModule(moduleCmpInfo, records)
-            //    )
-
-            //// Get ssdt config
-            //let config = ctx.ConfigGetter "ssdt"
-            //// Get namespace
-            //let ns = 
-            //    config 
-            //    |> Seq.tryFind(fun (key, value) -> key = "namespace")
-            //    |> Option.map (fun (key, value) -> string value)
-            //    |> Option.defaultValue "Ssdt"
-
-            //let namespaceOrModule =
-            //    { SynModuleOrNamespaceRcd.CreateNamespace(Ident.CreateLong ns)
-            //        with Declarations = nestedModules }
-
-            //[ namespaceOrModule ]
