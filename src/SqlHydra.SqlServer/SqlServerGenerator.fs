b9d5155361369160d2c09a8618e1dbd74486785c
Jordan Marr <jmarr@microdesk.com>
2021-05-23 02:25:27 -0400

    Updates to nuget pack config

--- src/SqlHydra.SqlServer/SqlServerGenerator.fs
+++ src/SqlHydra.SqlServer/SqlServerGenerator.fs
@@ -32,7 +32,7 @@ type SqlServerGenerator() =
 
             let assembly = System.Reflection.Assembly.GetExecutingAssembly()
             let assemblyDir = FileInfo(assembly.Location).DirectoryName
-            let exePath = Path.Combine(assemblyDir, "SqlHydra.SqlServer.exe")
+            let exePath = Path.Combine(assemblyDir, "Provider", "SqlHydra.SqlServer.exe")
             if not (File.Exists exePath) then failwithf "Unable to find provider: '%s'." exePath
 
             // Call exe to pull schema and save as json
