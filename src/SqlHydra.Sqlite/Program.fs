c7b5d7e2ac5364b27edea3ee823e1f982f9d9580
Jordan Marr <jmarr@microdesk.com>
2021-05-23 15:19:19 -0400

    Started to create a Sqlite provider.

---
+++ src/SqlHydra.Sqlite/Program.fs
@@ -0,0 +1,12 @@
+﻿open SqlHydra
+open SqlHydra.Sqlite
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [| connectionString; schemaOutputPath |] -> 
+        let schema = SqliteSchemaProvider.getSchema connectionString
+        Schema.serialize schemaOutputPath schema
+        0
+    | _ ->
+        1
---
+++ src/SqlHydra.Sqlite/SqliteSchemaProvider.fs
@@ -0,0 +1,66 @@
+﻿module SqlHydra.Sqlite.SqliteSchemaProvider
+
+open System.Data
+open Microsoft.Data.Sqlite
+open SqlHydra.Schema
+
+let getSchema (connectionString: string) : Schema = 
+    use conn = new SqliteConnection(connectionString)
+    conn.Open()
+    let sTables = conn.GetSchema("Tables")
+    let sColumns = conn.GetSchema("Columns")
+
+    let allColumns = 
+        sColumns.Rows
+        |> Seq.cast<DataRow>
+        |> Seq.map (fun col -> 
+            {| TableCatalog = col.["TABLE_CATALOG"] :?> string
+               TableSchema = col.["TABLE_SCHEMA"] :?> string
+               TableName = col.["TABLE_NAME"] :?> string
+               ColumnName = col.["COLUMN_NAME"] :?> string
+               DataType = col.["DATA_TYPE"] :?> string
+               IsNullable = 
+                match col.["IS_NULLABLE"] :?> string with 
+                | "YES" -> true
+                | _ -> false
+            |}
+        )
+
+    let tables = 
+        sTables.Rows
+        |> Seq.cast<DataRow>
+        |> Seq.map (fun tbl -> 
+            let tableCatalog = tbl.["TABLE_CATALOG"] :?> string
+            let tableSchema = tbl.["TABLE_SCHEMA"] :?> string
+            let tableName  = tbl.["TABLE_NAME"] :?> string
+            let tableType = tbl.["TABLE_TYPE"] :?> string
+
+            let columns = 
+                allColumns
+                |> Seq.filter (fun col -> 
+                    col.TableCatalog = tableCatalog && 
+                    col.TableSchema = tableSchema &&
+                    col.TableName = tableName
+                )
+                |> Seq.map (fun col -> 
+                    { Column.Name = col.ColumnName
+                      Column.IsNullable = col.IsNullable
+                      Column.DataType = col.DataType
+                      Column.ClrType = 
+                        SqliteDataTypes.tryFindMapping(col.DataType) 
+                        |> Option.map (fun mapping -> mapping.ClrType)
+                        |> Option.defaultValue "obj"
+                    }
+                )
+                |> Seq.toArray
+
+            { Table.Catalog = tableCatalog
+              Table.Schema = tableSchema
+              Table.Name =  tableName
+              Table.Type = if tableType = "BASE TABLE" then TableType.Table else TableType.View
+              Table.Columns = columns
+            }
+        )
+        |> Seq.toArray
+
+    { Tables = tables }
