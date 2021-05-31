ad9dbcca32a6a09f9cad632108251acb9ca94ef4
Jordan Marr <jmarr@microdesk.com>
2021-05-31 01:40:33 -0400

    Added CLIMutable atts to records

--- src/SqlHydra/SchemaGenerator.fs
+++ src/SqlHydra/SchemaGenerator.fs
@@ -1,35 +1,44 @@
 ﻿module SqlHydra.SchemaGenerator
 open FSharp.Compiler.SyntaxTree
+open FSharp.Compiler
 open FsAst
 open Fantomas
 open Schema
+
+let cliMutableAttribute = 
+    let attr =
+        { TypeName = LongIdentWithDots.CreateString "CLIMutable"
+          ArgExpr = SynExpr.CreateUnit
+          Target = None
+          AppliesToGetterAndSetter = false
+          Range = Range.range.Zero } : SynAttribute
+
+    let atts = [ SynAttributeList.Create(attr) ]
+    SynModuleDecl.CreateAttributes(atts)
     
-let toRecord (tbl: Table) = 
-    let recordCmpInfo = 
-        let myRecordId = LongIdentWithDots.CreateString tbl.Name
-        SynComponentInfoRcd.Create(myRecordId.Lid)
-
+let tableRecord (tbl: Table) = 
+    let myRecordId = LongIdentWithDots.CreateString tbl.Name
+    let recordCmpInfo = SynComponentInfoRcd.Create(myRecordId.Lid)
+    
     let recordDef =
-        let rcrd = 
-            tbl.Columns
-            |> Array.map (fun col -> 
-                let field = 
-                    if col.ClrType = "byte[]" then 
-                        let b = SynType.Create("byte")
-                        SynType.Array(0, b, FSharp.Compiler.Range.range.Zero)
-                    else
-                        SynType.Create(col.ClrType)
+        tbl.Columns
+        |> Array.map (fun col -> 
+            let field = 
+                if col.ClrType = "byte[]" then 
+                    let b = SynType.Create("byte")
+                    SynType.Array(0, b, Range.range.Zero)
+                else
+                    SynType.Create(col.ClrType)
                     
-                if col.IsNullable then                         
-                    let opt = SynType.Option(field)
-                    SynFieldRcd.Create(Ident.Create(col.Name), opt)
-                else 
-                    SynFieldRcd.Create(Ident.Create(col.Name), field)
-            )
-            |> Array.toList
-            |> SynTypeDefnSimpleReprRecordRcd.Create
-
-        SynTypeDefnSimpleReprRcd.Record rcrd
+            if col.IsNullable then                         
+                let opt = SynType.Option(field)
+                SynFieldRcd.Create(Ident.Create(col.Name), opt)
+            else 
+                SynFieldRcd.Create(Ident.Create(col.Name), field)
+        )
+        |> Array.toList
+        |> SynTypeDefnSimpleReprRecordRcd.Create
+        |> SynTypeDefnSimpleReprRcd.Record
         
     SynModuleDecl.CreateSimpleType(recordCmpInfo, recordDef)
 
@@ -38,13 +47,21 @@ let generateModule (ns: string) (schema: Schema) =
         schema.Tables
         |> Array.toList
         |> List.groupBy (fun t -> t.Schema)
-        |> List.map (fun (schema, tables) -> schema, tables |> List.map toRecord)
+        |> List.map (fun (schema, tables) -> schema, tables |> List.map tableRecord)
     
     let nestedModules = 
         recordsBySchema
-        |> List.map (fun (schema, records) -> 
-            let moduleCmpInfo = SynComponentInfoRcd.Create [ Ident.Create schema ]
-            SynModuleDecl.CreateNestedModule(moduleCmpInfo, records)
+        |> List.map (fun (schema, tableRecords) -> 
+            let schemaNestedModule = SynComponentInfoRcd.Create [ Ident.Create schema ]
+
+            let tableRecordDeclarations = 
+                [ for record in tableRecords do 
+
+                    yield cliMutableAttribute
+                    yield record 
+                ]
+
+            SynModuleDecl.CreateNestedModule(schemaNestedModule, tableRecordDeclarations)
         )
 
     let namespaceOrModule =
