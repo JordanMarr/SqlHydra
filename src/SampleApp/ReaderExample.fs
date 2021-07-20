29b833d02aa9d5bf7fa5c6aa07f10f4efa429f37
Jordan Marr <jmarr@microdesk.com>
2021-07-20 00:47:33 -0400

    Updated sample app reader examples

--- src/SampleApp/ReaderExample.fs
+++ src/SampleApp/ReaderExample.fs
@@ -28,11 +28,11 @@ let getProductNamesNumbers(conn: SqlConnection) = task {
     return [
         let productDataReader = SalesLT.ProductDataReader(reader)
         while reader.Read() do
-            productDataReader.Name(), productDataReader.ProductNumber()
+            productDataReader.Name.Read(), productDataReader.ProductNumber.Read()
     ]
 }
 
-let getCustomersWithAddresses_Readers(conn: SqlConnection) = task {
+let getCustomersJoinAddresses(conn: SqlConnection) = task {
     let sql = 
         """
         SELECT * FROM SalesLT.Customer c
@@ -53,10 +53,32 @@ let getCustomersWithAddresses_Readers(conn: SqlConnection) = task {
     ]
 }
 
+let getCustomersLeftJoinAddresses(conn: SqlConnection) = task {
+    let sql = 
+        """
+        SELECT TOP 20 * FROM SalesLT.Customer c
+        LEFT JOIN SalesLT.CustomerAddress ca ON c.CustomerID = ca.CustomerID
+        LEFT JOIN SalesLT.Address a on ca.AddressID = a.AddressID
+        WHERE c.CustomerID IN (
+            29485,29486, 29489, -- these have an a.AddressID, so LEFT JOIN should yield "Some"
+            1,2)                -- these do not have have an a.AddressID, so LEFT JOIN should yield "None"
+        ORDER BY c.CustomerID
+        """
+    use cmd = new SqlCommand(sql, conn)
+    use! reader = cmd.ExecuteReaderAsync()
+    let customer = SalesLT.CustomerDataReader(reader)
+    let address = SalesLT.AddressDataReader(reader)
+
+    return [
+        while reader.Read() do
+            customer.ToRecord(), address.ToRecordIf(address.AddressID)
+    ]
+}
+
 let getProductsAndCategories(conn: SqlConnection) = task {
     let sql = 
         """
-        SELECT TOP 4 *
+        SELECT *
         FROM SalesLT.Product p
         JOIN SalesLT.ProductCategory c ON p.ProductCategoryID = c.ProductCategoryID
         """
@@ -81,8 +103,11 @@ let runQueries() = task {
     let! productNamesNumbers = getProductNamesNumbers conn
     printfn "Product Names and Numbers: %A" productNamesNumbers
 
-    let! customersAddresses = getCustomersWithAddresses_Readers conn
-    printfn "Customers-Addresses: %A" customersAddresses
+    let! customersAddresses = getCustomersJoinAddresses conn
+    printfn "Customers-Join-Addresses: %A" customersAddresses
+
+    let! customerLeftJoinAddresses = getCustomersLeftJoinAddresses conn
+    printfn "Customer-LeftJoin-Addresses: %A" customerLeftJoinAddresses
 
     let! productsCategories = getProductsAndCategories conn
     printfn "Products-Categories: %A" productsCategories
