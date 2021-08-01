71735fa8461b3a12b4dc31da548a5899a230b9d7
Jordan Marr <jnmarr@hotmail.com>
2021-08-01 02:43:09 -0400

    Impl. skeleton wizard w/ SpectreConsole

--- src/SqlHydra/Console.fs
+++ src/SqlHydra/Console.fs
@@ -6,11 +6,20 @@ open System
 open Newtonsoft.Json
 
 let newConfigWizard() = 
+    let connStr = AnsiConsole.Ask<string>("Connection string:")
+    let outputFile = AnsiConsole.Ask<string>("Generated filename (Ex: AdventureWorks.fs):")
+    let ns = AnsiConsole.Ask<string>("Namespace:")
+    let isCLIMutable = 
+        let selection = SelectionPrompt<string>()
+        selection.Title <- "Add CLIMutable attribute to generated records?"
+        selection.AddChoices(["Yes"; "No"]) |> ignore
+        AnsiConsole.Prompt(selection)
+    
     { 
-        Config.ConnectionString = ""
-        Config.OutputFile = ""
-        Config.Namespace = ""
-        Config.IsCLIMutable = false
+        Config.ConnectionString = connStr
+        Config.OutputFile = outputFile
+        Config.Namespace = ns
+        Config.IsCLIMutable = isCLIMutable = "Yes"
         Config.Readers = 
             {
                 ReadersConfig.IsEnabled = false
@@ -18,12 +27,26 @@ let newConfigWizard() =
             }
     }
 
+let fileName = "hydra.json"
+
+let saveConfig (cfg: Config) = 
+    let json = JsonConvert.SerializeObject(cfg)
+    IO.File.WriteAllText(fileName, json)
+
+let tryLoadConfig() = 
+    if IO.File.Exists(fileName) then
+        try
+            let json = IO.File.ReadAllText(fileName)
+            JsonConvert.DeserializeObject<SqlHydra.Schema.Config>(json) |> Some
+        with ex -> None
+    else 
+        None
+
 let getConfig() = 
-    let fileName = "hydra.json"
-    let configExists = IO.File.Exists(fileName)
-    if configExists then
-        let json = IO.File.ReadAllText(fileName)
-        JsonConvert.DeserializeObject<SqlHydra.Schema.Config>(json)
-    else
-        newConfigWizard()
+    match tryLoadConfig() with
+    | Some cfg -> cfg
+    | None ->
+        let cfg = newConfigWizard()
+        saveConfig cfg
+        cfg
 
