830e8f89ac81c8ebc5a6d126aa8e0288155d1178
Jordan Marr <jmarr@microdesk.com>
2021-07-31 23:08:58 -0400

    Started moving config to json

---
+++ src/SqlHydra/Console.fs
@@ -0,0 +1,29 @@
+﻿module Console
+
+open Spectre.Console
+open SqlHydra.Schema
+open System
+open Newtonsoft.Json
+
+let newConfigWizard() = 
+    { 
+        Config.ConnectionString = ""
+        Config.OutputFile = ""
+        Config.Namespace = ""
+        Config.IsCLIMutable = false
+        Config.Readers = 
+            {
+                ReadersConfig.IsEnabled = false
+                ReadersConfig.ReaderType = ""
+            }
+    }
+
+let getConfig() = 
+    let fileName = "hydra.json"
+    let configExists = IO.File.Exists(fileName)
+    if configExists then
+        let json = IO.File.ReadAllText(fileName)
+        JsonConvert.DeserializeObject<SqlHydra.Schema.Config>(json)
+    else
+        newConfigWizard()
+
