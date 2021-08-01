0b989675a5f204dd1ef2503363eac07270818fda
Jordan Marr <jmarr@microdesk.com>
2021-08-01 19:35:57 -0400

    Improvements & fixes

--- src/SqlHydra/Console.fs
+++ src/SqlHydra/Console.fs
@@ -7,6 +7,12 @@ open Tomlyn
 open Tomlyn.Syntax
 open Tomlyn.Model
 
+type TomlTable with
+    member this.TryGetTable(name: string) =
+        if this.ContainsKey(name)
+        then Some (this.Item(name) :?> TomlTable)
+        else None
+
 type AppInfo = {
     Name: string
     Command: string
@@ -14,30 +20,36 @@ type AppInfo = {
     Version: string
 }
 
-type TomlConfigResult = 
+type LoadConfigResult = 
     | Valid of Config
     | Invalid of error: string
     | NotFound
 
+/// Creates a yes/no prmompt.
 let yesNo(title: string) = 
     let selection = SelectionPrompt<string>()
     selection.Title <- title
-    selection.AddChoices(["Yes"; "No"]) |> ignore
-    AnsiConsole.Prompt(selection) = "Yes"
+    selection.AddChoices(["Yes"; "No"]) |> ignore    
+    let answer = AnsiConsole.Prompt(selection)
+    if answer = "Yes"
+    then AnsiConsole.MarkupLine($"{title} [green]{answer}[/]")
+    else AnsiConsole.MarkupLine($"{title} [red]{answer}[/]")
+    answer = "Yes"
 
+/// Presents a series of user prompts to create a new config file.
 let newConfigWizard(app: AppInfo) = 
     let connection = AnsiConsole.Ask<string>("Enter a database [green]Connection String[/]:")
     let outputFile = AnsiConsole.Ask<string>("Enter an [green]Output Filename[/] (Ex: [yellow]AdventureWorks.fs[/]):")
     let ns = AnsiConsole.Ask<string>("Enter a [green]Namespace[/] (Ex: [yellow]MyApp.AdventureWorks[/]):")
-    let isCLIMutable = yesNo "Add CLIMutable attribute to generated records?"
+    let isCLIMutable = yesNo "Add CLIMutable attribute to generated records?"    
     let enableReaders = yesNo "Generate HydraReader?"
     let useDefaultReaderType = 
         if enableReaders 
         then yesNo $"Use the default Data Reader Type? (Default = {app.DefaultReaderType}):"
         else false
     let readerType = 
-        if not useDefaultReaderType
-        then AnsiConsole.Ask<string>($"Enter [green]Data Reader Type[/]:")
+        if enableReaders && not useDefaultReaderType
+        then AnsiConsole.Ask<string>($"Enter a [green]Data Reader Type[/]:")
         else app.DefaultReaderType
 
     { 
@@ -55,6 +67,7 @@ let newConfigWizard(app: AppInfo) =
 let buildTomlFilename(app: AppInfo) =
     $"{app.Command}.toml"
 
+/// Saves a config as toml.
 let saveConfig (tomlFilename: string, cfg: Config) = 
     let doc = DocumentSyntax()
 
@@ -73,17 +86,18 @@ let saveConfig (tomlFilename: string, cfg: Config) =
     let toml = doc.ToString()
     IO.File.WriteAllText(tomlFilename, toml)
 
+/// Reads a config from toml.
 let tryLoadConfig(tomlFileName: string) = 
     if IO.File.Exists(tomlFileName) then
         try
+            (*
+                NOTE: New configuration keys should be parsed gracefully so as to not break older versions!
+            *)
             let toml = IO.File.ReadAllText(tomlFileName)
             let doc = Toml.Parse(toml)
             let table = doc.ToModel()
             let general = table.Item("general") :?> TomlTable
-            let readers = 
-                table.Item("readers") 
-                |> Option.ofObj
-                |> Option.map (fun o -> o :?> TomlTable)
+            let readersMaybe = table.TryGetTable("readers")
 
             {
                 Config.ConnectionString = general.["connection"] :?> string
@@ -91,7 +105,7 @@ let tryLoadConfig(tomlFileName: string) =
                 Config.Namespace = general.["namespace"] :?> string
                 Config.IsCLIMutable = general.["cli_mutable"] :?> bool
                 Config.Readers = 
-                    readers
+                    readersMaybe
                     |> Option.map (fun tbl -> 
                         {
                             ReadersConfig.ReaderType = tbl.["reader_type"] :?> string
