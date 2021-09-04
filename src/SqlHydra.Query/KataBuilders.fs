5907cffdf854ad21bef1ca56becedff5cb94bbd8
Jordan Marr <jmarr@microdesk.com>
2021-09-04 15:24:49 -0400

    Refactored to query fns to remove magic strings from visitors

--- src/SqlHydra.Query/KataBuilders.fs
+++ src/SqlHydra.Query/KataBuilders.fs
@@ -338,27 +338,3 @@ let delete<'T> = DeleteExpressionBuilder<'T>()
 let insert<'T> = InsertExpressionBuilder<'T>()
 let update<'T> = UpdateExpressionBuilder<'T>()
 
-/// WHERE column is IN values
-let isIn<'P> (prop: 'P) (values: 'P list) = true
-/// WHERE column is IN values
-let inline (|=|) (prop: 'P) (values: 'P list) = true
-
-/// WHERE column is NOT IN values
-let isNotIn<'P> (prop: 'P) (values: 'P list) = true
-/// WHERE column is NOT IN values
-let inline (|<>|) (prop: 'P) (values: 'P list) = true
-
-/// WHERE column like value   
-let like<'P> (prop: 'P) (pattern: string) = true
-/// WHERE column like value   
-let inline (=%) (prop: 'P) (pattern: string) = true
-
-/// WHERE column not like value   
-let notLike<'P> (prop: 'P) (pattern: string) = true
-/// WHERE column not like value   
-let inline (<>%) (prop: 'P) (pattern: string) = true
-
-/// WHERE column IS NULL
-let isNullValue<'P> (prop: 'P) = true
-/// WHERE column IS NOT NULL
-let isNotNullValue<'P> (prop: 'P) = true
