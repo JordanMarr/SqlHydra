7a9a1978c88a86859382a72293c0e39f8e29c443
Jordan Marr <jmarr@microdesk.com>
2021-09-17 01:42:19 -0400

    Added LOCALHOST compiler directive to Npgsql conn strings

--- src/Tests/Npgsql/DB.fs
+++ src/Tests/Npgsql/DB.fs
@@ -1,8 +1,7 @@
 ﻿module Npgsql.DB
 
-// devcontainer: "npgsql"
-//let connectionString = @"Server=npgsql;Port=5432;Database=Adventureworks;User Id=postgres;Password=postgres;"
-
-
-// localhost
+#if LOCALHOST // localhost
 let connectionString = @"Server=localhost;Port=54320;Database=Adventureworks;User Id=postgres;Password=postgres;"
+#else // devcontainer: "npgsql"
+let connectionString = @"Server=npgsql;Port=5432;Database=Adventureworks;User Id=postgres;Password=postgres;"
+#endif
