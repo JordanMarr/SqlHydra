db7ce5f16ad7fef104e249471f235f26bf774595
Jordan Marr <jmarr@microdesk.com>
2022-02-22 01:51:39 -0500

    Replaced advworks with OT sample database

---
+++ src/.devcontainer/oracle/oracle-sample-database/ot_drop.sql
@@ -0,0 +1,22 @@
+--------------------------------------------------------------------------------------
+-- Name	       : OT (Oracle Tutorial) Sample Database
+-- Link	       : http://www.oracletutorial.com/oracle-sample-database/
+-- Version     : 1.0
+-- Last Updated: July-28-2017
+-- Copyright   : Copyright © 2017 by www.oracletutorial.com. All Rights Reserved.
+-- Notice      : Use this sample database for the educational purpose only.
+--               Credit the site oracletutorial.com explitly in your materials that
+--               use this sample database.
+--------------------------------------------------------------------------------------
+DROP TABLE ot.order_items;  
+DROP TABLE ot.orders;
+DROP TABLE ot.inventories;
+DROP TABLE ot.products;
+DROP TABLE ot.product_categories;
+DROP TABLE ot.warehouses;
+DROP TABLE ot.employees;
+DROP TABLE ot.contacts;
+DROP TABLE ot.customers;
+DROP TABLE ot.locations;
+DROP TABLE ot.countries;
+DROP TABLE ot.regions;
