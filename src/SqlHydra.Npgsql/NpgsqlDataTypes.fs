a364f544e1682e87f63fec53652b69691b12e2fe
Jordan Marr <JordanMarr@users.noreply.github.com>
2022-11-16 13:23:20 -0500

    v1.0.5 - added support for other npgsql array types.

--- src/SqlHydra.Npgsql/NpgsqlDataTypes.fs
+++ src/SqlHydra.Npgsql/NpgsqlDataTypes.fs
@@ -8,55 +8,64 @@ let private r : Npgsql.NpgsqlDataReader = null
 
 /// A list of supported column type mappings
 let supportedTypeMappings =
-    [// ColumnTypeAlias                 ClrType                 DbType              ProviderDbType                      ReaderMethod
-        "boolean",                      "bool",                 DbType.Boolean,     None,                               nameof r.GetBoolean
-        "smallint",                     "int16",                DbType.Int16,       None,                               nameof r.GetInt16
-        "integer",                      "int",                  DbType.Int32,       None,                               nameof r.GetInt32
-        "bigint",                       "int64",                DbType.Int64,       None,                               nameof r.GetInt64
-        "real",                         "double",               DbType.Double,      None,                               nameof r.GetDouble
-        "double precision",             "double",               DbType.Double,      None,                               nameof r.GetDouble
-        "numeric",                      "decimal",              DbType.Decimal,     None,                               nameof r.GetDecimal
-        "money",                        "decimal",              DbType.Decimal,     None,                               nameof r.GetDecimal
-        "text",                         "string",               DbType.String,      None,                               nameof r.GetString
-        "character varying",            "string",               DbType.String,      None,                               nameof r.GetString
-        "character",                    "string",               DbType.String,      None,                               nameof r.GetString
-        "citext",                       "string",               DbType.String,      None,                               nameof r.GetString
-        "json",                         "string",               DbType.String,      Some (nameof NpgsqlDbType.Json),    nameof r.GetString
-        "jsonb",                        "string",               DbType.String,      Some (nameof NpgsqlDbType.Jsonb),   nameof r.GetString
-        "xml",                          "string",               DbType.String,      None,                               nameof r.GetString
+    [// ColumnTypeAlias                 ClrType                 DbType              ProviderDbType                      ReaderMethod            ArrayBaseType
+        "boolean",                      "bool",                 DbType.Boolean,     None,                               nameof r.GetBoolean,    Some NpgsqlDbType.Boolean
+        "smallint",                     "int16",                DbType.Int16,       None,                               nameof r.GetInt16,      Some NpgsqlDbType.Smallint
+        "integer",                      "int",                  DbType.Int32,       None,                               nameof r.GetInt32,      Some NpgsqlDbType.Integer
+        "bigint",                       "int64",                DbType.Int64,       None,                               nameof r.GetInt64,      Some NpgsqlDbType.Bigint
+        "real",                         "double",               DbType.Double,      None,                               nameof r.GetDouble,     Some NpgsqlDbType.Real
+        "double precision",             "double",               DbType.Double,      None,                               nameof r.GetDouble,     Some NpgsqlDbType.Double
+        "numeric",                      "decimal",              DbType.Decimal,     None,                               nameof r.GetDecimal,    Some NpgsqlDbType.Numeric
+        "money",                        "decimal",              DbType.Decimal,     None,                               nameof r.GetDecimal,    Some NpgsqlDbType.Money
+        "text",                         "string",               DbType.String,      None,                               nameof r.GetString,     Some NpgsqlDbType.Text
+        "character varying",            "string",               DbType.String,      None,                               nameof r.GetString,     None
+        "character",                    "string",               DbType.String,      None,                               nameof r.GetString,     None
+        "citext",                       "string",               DbType.String,      None,                               nameof r.GetString,     None
+        "json",                         "string",               DbType.String,      Some (nameof NpgsqlDbType.Json),    nameof r.GetString,     None
+        "jsonb",                        "string",               DbType.String,      Some (nameof NpgsqlDbType.Jsonb),   nameof r.GetString,     None
+        "xml",                          "string",               DbType.String,      None,                               nameof r.GetString,     None
         // skipped unsupported types
-        "bit(1)",                       "bool",                 DbType.Boolean,     None,                               nameof r.GetBoolean
+        "bit(1)",                       "bool",                 DbType.Boolean,     None,                               nameof r.GetBoolean,    Some NpgsqlDbType.Bit
         // skipped unsupported types
-        "uuid",                         "System.Guid",          DbType.Guid,        None,                               nameof r.GetGuid
+        "uuid",                         "System.Guid",          DbType.Guid,        None,                               nameof r.GetGuid,       Some NpgsqlDbType.Uuid
         // skipped unsupported types
-        "interval",                     "System.TimeSpan",      DbType.Time,        None,                               nameof r.GetTimeSpan
+        "interval",                     "System.TimeSpan",      DbType.Time,        None,                               nameof r.GetTimeSpan,   Some NpgsqlDbType.Interval
 #if NET5_0
-        "date",                         "System.DateTime",      DbType.DateTime,    None,                               nameof r.GetDateTime 
-        "time without time zone",       "System.TimeSpan",      DbType.Time,        None,                               nameof r.GetTimeSpan 
+        "date",                         "System.DateTime",      DbType.DateTime,    None,                               nameof r.GetDateTime,   Some NpgsqlDbType.Date
+        "time without time zone",       "System.TimeSpan",      DbType.Time,        None,                               nameof r.GetTimeSpan,   None        
 #endif
 #if NET6_0_OR_GREATER
-        "date",                         "System.DateOnly",      DbType.DateTime,    None,                               "GetDateOnly"
-        "time without time zone",       "System.TimeOnly",      DbType.Time,        None,                               "GetTimeOnly"
+        "date",                         "System.DateOnly",      DbType.DateTime,    None,                               "GetDateOnly",          Some NpgsqlDbType.Date
+        "time without time zone",       "System.TimeOnly",      DbType.Time,        None,                               "GetTimeOnly",          Some NpgsqlDbType.Time
 #endif
-        "timestamp with time zone",     "System.DateTime",      DbType.DateTime,    None,                               nameof r.GetDateTime 
-        "timestamp without time zone",  "System.DateTime",      DbType.DateTime,    None,                               nameof r.GetDateTime 
-        "time with time zone",          "System.DateTime",      DbType.DateTime,    None,                               nameof r.GetDateTime 
-        "bytea",                        "byte[]",               DbType.Binary,      None,                               nameof r.GetValue 
+        "timestamp with time zone",     "System.DateTime",      DbType.DateTime,    None,                               nameof r.GetDateTime,   Some NpgsqlDbType.TimestampTz 
+        "timestamp without time zone",  "System.DateTime",      DbType.DateTime,    None,                               nameof r.GetDateTime,   Some NpgsqlDbType.Timestamp
+        "time with time zone",          "System.DateTime",      DbType.DateTime,    None,                               nameof r.GetDateTime,   Some NpgsqlDbType.TimeTz
+        "bytea",                        "byte[]",               DbType.Binary,      None,                               nameof r.GetValue,      None
         // skipped unsupported types
-        "name",                         "string",               DbType.String,      None,                               nameof r.GetString
-        "(internal) char",              "char",                 DbType.String,      None,                               nameof r.GetChar
+        "name",                         "string",               DbType.String,      None,                               nameof r.GetString,     None
+        "(internal) char",              "char",                 DbType.String,      None,                               nameof r.GetChar,       Some NpgsqlDbType.Char
         // skipped unsupported types
-
-        // "Text,Array" can be parsed by Enum.Parse.
-        let textArray = $"{nameof NpgsqlDbType.Text},{nameof NpgsqlDbType.Array}"
-        "text[]",                       "string[]",             DbType.String,      Some textArray,                     nameof r.GetFieldValue
-        
-        let integerArray = $"{nameof NpgsqlDbType.Integer},{nameof NpgsqlDbType.Array}"
-        "integer[]",                    "int[]",                DbType.Int32,       Some integerArray,                  nameof r.GetFieldValue
     ]
+    /// Programmically add array mappings (where ArrayType is Some)
+    |> List.collect (fun (columnTypeAlias, clrType, dbType, providerDbType, readerMethod, arrayBaseType) ->
+        [
+            columnTypeAlias, clrType, dbType, providerDbType, readerMethod
+            if arrayBaseType.IsSome then
+                let npgsqlDbType = arrayBaseType.Value
+                yield!
+                    [ for suffix in [ "[]"; " []"; " array" ] do
+                        let columnArrayTypeAlias = $"{columnTypeAlias}{suffix}"                         // ex: "text" becomes: "text[]", "text []", "text array"
+                        let clrArrayType = $"{clrType}[]"                                               // ex: "string" becomes: "string[]"
+                        let providerDbArrayType = Some $"{npgsqlDbType},{nameof NpgsqlDbType.Array}"    // ex: "Text,Array" (which can be parsed by Enum.Parse when setting parameter NpgsqlDbType property)
+                        let readerMethodArray = nameof r.GetFieldValue
+                        columnArrayTypeAlias, clrArrayType, dbType, providerDbArrayType, readerMethodArray ]
+        ]
+    )
 
 let typeMappingsByName =
     supportedTypeMappings
+    
     |> List.map (fun (columnTypeAlias, clrType, dbType, providerDbType, readerMethod) ->
         columnTypeAlias,
         { 
