0937739b83fda1ab3773ee1d020d544a5a58f545
Jordan Marr <jmarr@microdesk.com>
2023-11-05 14:50:06 -0500

    Converted SqlServer.SelectTaskTests to NUnit

--- src/Tests/SqlServer/SelectTaskTests.fs
+++ src/Tests/SqlServer/SelectTaskTests.fs
@@ -1,7 +1,8 @@
-﻿module SqlServer.SelectTaskTests
+﻿module SqlServer.``selectTask Tests``
 
 open SqlHydra.Query
 open Expecto
+open NUnit.Framework
 open DB
 
 #if NET6_0
@@ -16,109 +17,111 @@ let openContext() =
     let conn = openConnection()
     new QueryContext(conn, compiler)
 
-[<Tests>]
-let selectTests = 
-    categoryList "SqlServer" "selectTask" [
-        
-        testTask "selectTask" {
-            let! results = 
-                selectTask HydraReader.Read (Create openContext) {
-                    for p in Person.Person do
-                    take 10
-                }
-        
-            gt0 results
+[<Test>]
+let ``select Task``() = task {
+    let! results = 
+        selectTask HydraReader.Read (Create openContext) {
+            for p in Person.Person do
+            take 10
         }
         
-        testTask "selectTask - select" {
-            let! results = 
-                selectTask HydraReader.Read (Create openContext) {
-                    for p in Person.Person do
-                    take 10
-                    select p
-                }
-        
-            gt0 results
-        }
+    gt0 results
+}
 
-        testTask "selectTask - toArray" {
-            let! results = 
-                selectTask HydraReader.Read (Create openContext) {
-                    for p in Person.Person do
-                    take 10
-                    toArray
-                }
-        
-            gt0 results
+[<Test>]
+let ``selectTask - select p``() = task {
+    let! results = 
+        selectTask HydraReader.Read (Create openContext) {
+            for p in Person.Person do
+            take 10
+            select p
         }
         
-        testTask "selectTask - mapList column" {
-            let! results = 
-                selectTask HydraReader.Read (Create openContext) {
-                    for p in Person.Person do
-                    take 10
-                    mapList p.FirstName
-                }
-                
-            gt0 results
-        }
+    gt0 results
+}
 
-        testTask "selectTask - select entity - mapSeq column" {
-            let! results = 
-                selectTask HydraReader.Read (Create openContext) {
-                    for p in Person.Person do
-                    take 10
-                    select p
-                    mapSeq $"{p.FirstName} {p.LastName}"
-                }
-        
-            gt0 results
+[<Test>]
+let ``selectTask - toArray``() = task {
+    let! results = 
+        selectTask HydraReader.Read (Create openContext) {
+            for p in Person.Person do
+            take 10
+            toArray
         }
         
-        testTask "selectTask - select columns into - mapList column" {
-            let! results = 
-                selectTask HydraReader.Read (Create openContext) {
-                    for p in Person.Person do
-                    take 10
-                    select (p.FirstName, p.LastName) into (fname, lname)
-                    mapList $"{fname} {lname}"
-                }
-                
-            gt0 results
-        }
-        
-        testTask "selectTask - count" {
-            let! results = 
-                selectTask HydraReader.Read (Create openContext) {
-                    for p in Person.Person do
-                    count
-                }
-        
-            Expect.isTrue (results > 0) ""
-        }
+    gt0 results
+}
 
-        testTask "selectTask - tryHead - Selected" {
-            let! result = 
-                selectTask HydraReader.Read (Create openContext) {
-                    for p in Person.Person do
-                    take 1
-                    tryHead
-                }
-        
-            Expect.isSome result ""
+[<Test>]
+let ``selectTask - mapList column``() = task {
+    let! results = 
+        selectTask HydraReader.Read (Create openContext) {
+            for p in Person.Person do
+            take 10
+            mapList p.FirstName
         }
+        
+    gt0 results
+}
 
-        testTask "selectTask - tryHead - Mapped" {
-            let! result = 
-                selectTask HydraReader.Read (Create openContext) {
-                    for p in Person.Person do
-                    take 1
-                    mapSeq $"{p.FirstName} {p.LastName}"
-                    tryHead
-                }
-        
-            Expect.isSome result ""
+[<Test>]
+let ``selectTask - select entity - mapSeq column``() = task {
+    let! results = 
+        selectTask HydraReader.Read (Create openContext) {
+            for p in Person.Person do
+            take 10
+            select p
+            mapSeq $"{p.FirstName} {p.LastName}"
         }
         
-    ]
-    
+    gt0 results
+}
+
+[<Test>]
+let ``selectTask - select columns into - mapList column``() = task {
+    let! results = 
+        selectTask HydraReader.Read (Create openContext) {
+            for p in Person.Person do
+            take 10
+            select (p.FirstName, p.LastName) into (fname, lname)
+            mapList $"{fname} {lname}"
+        }
+        
+    gt0 results
+}
+
+[<Test>]
+let ``selectTask - count``() = task {
+    let! results = 
+        selectTask HydraReader.Read (Create openContext) {
+            for p in Person.Person do
+            count
+        }
+        
+    Expect.isTrue (results > 0) ""
+}
+
+[<Test>]
+let ``selectTask - tryHead - Selected``() = task {
+    let! result = 
+        selectTask HydraReader.Read (Create openContext) {
+            for p in Person.Person do
+            take 1
+            tryHead
+        }
+        
+    Expect.isSome result ""
+}
+
+[<Test>]
+let ``selectTask - tryHead - Mapped``() = task {
+    let! result = 
+        selectTask HydraReader.Read (Create openContext) {
+            for p in Person.Person do
+            take 1
+            mapSeq $"{p.FirstName} {p.LastName}"
+            tryHead
+        }
+        
+    Expect.isSome result ""
+}
