2281bcc8ade9a5724c7e3dc515373a71666589c0
Jordan Marr <jmarr@microdesk.com>
2023-11-05 15:27:58 -0500

    Converted UnitTest.ColumnFilters to NUnit

--- src/Tests/UnitTests/ColumnFilters.fs
+++ src/Tests/UnitTests/ColumnFilters.fs
@@ -1,90 +1,85 @@
-﻿module UnitTests.ColumnFilters
+﻿module UnitTests.``Column Filters``
 
 open Expecto
+open NUnit.Framework
 open SqlHydra.Domain
 open SqlHydra.SchemaFilters
 
-[<Tests>]
-let tests =     
+let col nm = 
+    {
+        Name = nm
+        TypeMapping = { ClrType = ""; ColumnTypeAlias = ""; DbType = System.Data.DbType.String; ReaderMethod = ""; ProviderDbType = None }
+        IsNullable = false
+        IsPK = false
+    }
 
-    let col nm = 
-        {
-            Name = nm
-            TypeMapping = { ClrType = ""; ColumnTypeAlias = ""; DbType = System.Data.DbType.String; ReaderMethod = ""; ProviderDbType = None }
-            IsNullable = false
-            IsPK = false
-        }
+let equalLists lst1 lst2 = 
+    Expect.equal (Set lst1) (Set lst2) "Lists are not equal"
 
-    let equalLists lst1 lst2 = 
-        Expect.equal (Set lst1) (Set lst2) "Lists are not equal"
+[<Test>]
+let ``Include All and No Excludes``() = 
+    let idCol = col "ID"
+    let fnameCol = col "FName"
+    let lnameCol = col "LName"
+    let ageCol = col "Age"
+    let columns = [ idCol; fnameCol; lnameCol; ageCol ]
+    
+    let filters = { 
+        Includes = [ "*.*" ]
+        Excludes = [ ] 
+        Restrictions = Map.empty
+    }
 
-    categoryList "Unit Tests" "Column Filters" [
-        
-        test "Include All and No Excludes" {
-            let idCol = col "ID"
-            let fnameCol = col "FName"
-            let lnameCol = col "LName"
-            let ageCol = col "Age"
-            let columns = [ idCol; fnameCol; lnameCol; ageCol ]
-            
-            let filters = { 
-                Includes = [ "*.*" ]
-                Excludes = [ ] 
-                Restrictions = Map.empty
-            }
+    let filteredColumns = columns |> filterColumns filters "dbo" "Person"
+    equalLists columns filteredColumns
 
-            let filteredColumns = columns |> filterColumns filters "dbo" "Person"
-            equalLists columns filteredColumns
-        }
+[<Test>]
+let ``Include All and Exclude FName and LName``() = 
+    let idCol = col "ID"
+    let fnameCol = col "FName"
+    let lnameCol = col "LName"
+    let ageCol = col "Age"
+    let columns = [ idCol; fnameCol; lnameCol; ageCol ]
+    
+    let filters = { 
+        Includes = [ "*.*" ]
+        Excludes = [ "dbo/Person.FName"; "*/Person.LName" ] 
+        Restrictions = Map.empty
+    }
 
-        test "Include All and Exclude FName and LName" {
-            let idCol = col "ID"
-            let fnameCol = col "FName"
-            let lnameCol = col "LName"
-            let ageCol = col "Age"
-            let columns = [ idCol; fnameCol; lnameCol; ageCol ]
-            
-            let filters = { 
-                Includes = [ "*.*" ]
-                Excludes = [ "dbo/Person.FName"; "*/Person.LName" ] 
-                Restrictions = Map.empty
-            }
+    let filteredColumns = columns |> filterColumns filters "dbo" "Person"
+    equalLists filteredColumns [ idCol; ageCol ]
 
-            let filteredColumns = columns |> filterColumns filters "dbo" "Person"
-            equalLists filteredColumns [ idCol; ageCol ]
-        }
+[<Test>]
+let ``Ignore filter if no table match``() = 
+    let idCol = col "ID"
+    let fnameCol = col "FName"
+    let lnameCol = col "LName"
+    let ageCol = col "Age"
+    let columns = [ idCol; fnameCol; lnameCol; ageCol ]
+    
+    let filters = { 
+        Includes = [ "*.*" ]
+        Excludes = [ "Instrument.Age" ] 
+        Restrictions = Map.empty
+    }
 
-        test "Ignore filter if no table match" {
-            let idCol = col "ID"
-            let fnameCol = col "FName"
-            let lnameCol = col "LName"
-            let ageCol = col "Age"
-            let columns = [ idCol; fnameCol; lnameCol; ageCol ]
-            
-            let filters = { 
-                Includes = [ "*.*" ]
-                Excludes = [ "Instrument.Age" ] 
-                Restrictions = Map.empty
-            }
+    let filteredColumns = columns |> filterColumns filters "dbo" "Person"
+    equalLists filteredColumns columns
 
-            let filteredColumns = columns |> filterColumns filters "dbo" "Person"
-            equalLists filteredColumns columns
-        }
+[<Test>]
+let ``Exclude Only Underscore Columns``() = 
+    let idCol = col "ID"
+    let fnameCol = col "_FName"
+    let lnameCol = col "_LName"
+    let ageCol = col "Age"
+    let columns = [ idCol; fnameCol; lnameCol; ageCol ]
+    
+    let filters = { 
+        Includes = [ "*.*" ]
+        Excludes = [ "*._*" ] 
+        Restrictions = Map.empty
+    }
 
-        test "Exclude Only Underscore Columns" {
-            let idCol = col "ID"
-            let fnameCol = col "_FName"
-            let lnameCol = col "_LName"
-            let ageCol = col "Age"
-            let columns = [ idCol; fnameCol; lnameCol; ageCol ]
-            
-            let filters = { 
-                Includes = [ "*.*" ]
-                Excludes = [ "*._*" ] 
-                Restrictions = Map.empty
-            }
-
-            let filteredColumns = columns |> filterColumns filters "dbo" "Person"
-            equalLists filteredColumns [ idCol; ageCol ]
-        }
-    ]
+    let filteredColumns = columns |> filterColumns filters "dbo" "Person"
+    equalLists filteredColumns [ idCol; ageCol ]
