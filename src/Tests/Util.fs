8a6d5791796d92ed69c462455766b8edf3891e90
Jordan Marr <jmarr@microdesk.com>
2023-11-05 15:40:29 -0500

    Converted Util to NUnit

--- src/Tests/Util.fs
+++ src/Tests/Util.fs
@@ -1,16 +1,8 @@
 ﻿[<AutoOpen>]
 module Util
 
-open Expecto
-
-let sequencedTestList nm = testList nm >> testSequenced
-let fsequencedTestList nm = ftestList nm >> testSequenced
-
-let categoryList cat subCat tests =
-    testList cat [
-        sequencedTestList subCat tests
-    ]
+open NUnit.Framework
 
 /// Sequence length is > 0.
 let gt0 (items: 'Item seq) =
-    Expect.isTrue (items |> Seq.length > 0) "Expected more than 0."
+    Assert.IsTrue(items |> Seq.length > 0, "Expected more than 0.")
