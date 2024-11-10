1848309fd22cad0548b5d75533afa44e7083216c
Jordan Marr <jmarr@microdesk.com>
2024-11-10 17:03:27 -0500

    Started schema gen template rewrite

---
+++ src/Tests/UnitTests/SchemaTemplateTests.fs
@@ -0,0 +1,13 @@
+﻿module UnitTests.SchemaTemplateTests
+
+open NUnit.Framework
+open SqlHydra.Domain
+open SqlHydra
+
+[<Test>]
+let ``Schema Template Test`` () = 
+    let cfg = Npgsql.Generation.cfg
+    let info = SqlHydra.Npgsql.AppInfo.info
+    let schema = Npgsql.NpgsqlSchemaProvider.getSchema cfg
+    let output = SchemaTemplate.generate cfg info schema "1.0.0"
+    printfn $"Output:\n{output}"
